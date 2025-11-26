5558e64dab8dfdb27639a4eda683ab1cccf8f518
abyrowenTimoti <abyrowen@gmail.com>
2025-11-26 12:59:59 +0700

    commit

---
+++ pengunjungKafe01.java
@@ -0,0 +1,14 @@
+public class pengunjungKafe01 {
+  static void daftarPengunjung(String... namaPengunjung){
+    System.out.println("Daftar nama pengunjung: ");
+    for(String pengunjung : namaPengunjung){
+      System.out.println("- " + pengunjung);
+    }
+  }
+
+  public static void main(String[] args) {
+    daftarPengunjung("Ali", "Budi", "Citra");
+    daftarPengunjung();
+    daftarPengunjung("Doni", "Eti", "Fahmi", "Galih");
+  }
+}
